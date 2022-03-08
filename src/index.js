fa33ecad357f3367568d62054ccec7f0f42af072
Alexsandro Carvalho de Freitas <ALEXSANDRO.SYSMAP@fleetcor.com.br>
2022-03-08 15:54:17 -0300

    third commit

--- src/index.js
+++ src/index.js
@@ -1,43 +1,100 @@
-const express = require('express')
-const {v4:uuidv4 } = require("uuid")
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
-const customers = []
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.post("/account", (request, response)=> {
-    const {name, cpf} = request.body
-   
-    const customersAlreadyExists = customers.some(
-       (customer)=> customer.cpf === cpf
-       )
+const customers = [];
 
-   if (customersAlreadyExists){
-       response.status(400).json({ error:"cpf already registered!"})
-   }
-    
+//middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+	const { cpf } = request.headers;
 
-    customers.push({
-        cpf,
-        name,
-        id: uuidv4(),
-        statement:[],
-    })
-return response.status(201).send()
-})
+	const customer = customers.find((customer) => customer.cpf === cpf);
 
-app.get("/statement", (request, response) => {
- const { cpf } = request.headers
+	if (!customer) {
+		return response.status(400).json({ error: 'cpf already registered!' });
+	}
 
- const customer = customers.find((customer) => customer.cpf === cpf )
+	request.customer = customer;
 
-if (!customer) {
-  return response.status(400).json({ error: "Customer not found"})
+	return next();
 }
 
+function getBalance(statement) {
+	const balance = statement.reduce((acc, operation) => {
+		if (operation === 'credit') {
+			return acc + operation.amount;
+		} else {
+			return acc - operation.amount;
+		}
+	}, 0);
+	return balance;
+}
 
- return response.json(customer.statement)
-} )
+app.post('/account', (request, response) => {
+	const { name, cpf } = request.body;
 
+	const customersAlreadyExists = customers.some(
+		(customer) => customer.cpf === cpf
+	);
 
-app.listen(3333)
+	if (customersAlreadyExists) {
+		return response.status(400).json({ error: 'customer already exists!' });
+	}
+
+	const id = uuidv4;
+	customers.push({
+		cpf,
+		name,
+		id: uuidv4(),
+		statement: [],
+	});
+	return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+	const { customer } = request;
+
+	return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+	const { description, amount } = request.body;
+
+	const { customer } = request;
+
+	const statementOperation = {
+		description,
+		amount,
+		created_at: new Date(),
+		type: 'credit',
+	};
+
+	customer.statement.push(statementOperation);
+
+	return response.status(201).send;
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+	const { amount } = request.body;
+	const { customer } = request;
+
+	const balance = getBalance(customer.statement);
+
+	if (balance < amount) {
+		response.status(400).json({ error: 'insufficiente fundds!' });
+	}
+
+	const statementOperation = {
+		amount,
+		created_at: new Date(),
+		type: 'debit',
+	};
+
+	customer.statement.push(statementOperation);
+
+	return response(201).send();
+});
+
+app.listen(3333);
